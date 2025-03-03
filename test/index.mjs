bf696ddd7d3f3923cc4a4902b346f62e6e6b60bc
wangpan <pan.wang@ushow.media>
2025-03-03 15:48:17 +0800

    update

--- test/index.mjs
+++ test/index.mjs
@@ -2,12 +2,16 @@
  * @Author: wangpan pan.wang@ushow.media
  * @Date: 2025-03-02 16:09:10
  * @LastEditors: wangpan pan.wang@ushow.media
- * @LastEditTime: 2025-03-02 23:46:52
+ * @LastEditTime: 2025-03-03 15:27:16
  * @FilePath: /change-node-version/test/index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 // import path from 'node:path';
 
+const testFunc = () => {
+  console.log('testFunc');
+}
+testFunc()
 const obj = {
   name: ''
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: wangpan pan.wang@ushow.media
  * @Date: 2025-03-01 17:52:47
  * @LastEditors: wangpan pan.wang@ushow.media
- * @LastEditTime: 2025-03-03 00:02:22
+ * @LastEditTime: 2025-03-03 15:45:13
  * @FilePath: /change-node-version/webpack.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,6 +12,9 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'main.js',
+    environment: {
+      arrowFunction: false
+    },
     clean: true,
   },
   target: 'node',
@@ -31,11 +34,16 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env']
+            ]
+          }
         },
       },
     ],
   },
   optimization: {
-    minimize: true,
+    minimize: false,
   },
 }
