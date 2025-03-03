dd9bd1fbf70a68405879c3016e88e196bb174c39
wangpan <pan.wang@ushow.media>
2025-03-03 15:53:38 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: wangpan pan.wang@ushow.media
  * @Date: 2025-03-01 17:52:47
  * @LastEditors: wangpan pan.wang@ushow.media
- * @LastEditTime: 2025-03-03 15:45:13
+ * @LastEditTime: 2025-03-03 15:53:29
  * @FilePath: /change-node-version/webpack.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -44,6 +44,6 @@ module.exports = {
     ],
   },
   optimization: {
-    minimize: false,
+    minimize: true,
   },
 }
